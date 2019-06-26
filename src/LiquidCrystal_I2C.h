ceb682945a7cd6c1ac9a9e94fa1d624ff493cd2b
Bengt Martensson <barf@bengt-martensson.de>
2019-06-26 10:57:17 +0200

    Replace #define's with private static consts and some Arduino B* with gcc binary literals.

--- src/LiquidCrystal_I2C.h
+++ src/LiquidCrystal_I2C.h
@@ -4,53 +4,53 @@
 #include "Print.h" 
 #include <Wire.h>
 
-// commands
-#define LCD_CLEARDISPLAY 0x01
-#define LCD_RETURNHOME 0x02
-#define LCD_ENTRYMODESET 0x04
-#define LCD_DISPLAYCONTROL 0x08
-#define LCD_CURSORSHIFT 0x10
-#define LCD_FUNCTIONSET 0x20
-#define LCD_SETCGRAMADDR 0x40
-#define LCD_SETDDRAMADDR 0x80
-
-// flags for display entry mode
-#define LCD_ENTRYRIGHT 0x00
-#define LCD_ENTRYLEFT 0x02
-#define LCD_ENTRYSHIFTINCREMENT 0x01
-#define LCD_ENTRYSHIFTDECREMENT 0x00
-
-// flags for display on/off control
-#define LCD_DISPLAYON 0x04
-#define LCD_DISPLAYOFF 0x00
-#define LCD_CURSORON 0x02
-#define LCD_CURSOROFF 0x00
-#define LCD_BLINKON 0x01
-#define LCD_BLINKOFF 0x00
-
-// flags for display/cursor shift
-#define LCD_DISPLAYMOVE 0x08
-#define LCD_CURSORMOVE 0x00
-#define LCD_MOVERIGHT 0x04
-#define LCD_MOVELEFT 0x00
-
-// flags for function set
-#define LCD_8BITMODE 0x10
-#define LCD_4BITMODE 0x00
-#define LCD_2LINE 0x08
-#define LCD_1LINE 0x00
-#define LCD_5x10DOTS 0x04
-#define LCD_5x8DOTS 0x00
-
-// flags for backlight control
-#define LCD_BACKLIGHT 0x08
-#define LCD_NOBACKLIGHT 0x00
-
-#define En B00000100  // Enable bit
-#define Rw B00000010  // Read/Write bit
-#define Rs B00000001  // Register select bit
-
 class LiquidCrystal_I2C : public Print {
+    // commands
+    static const unsigned int LCD_CLEARDISPLAY = 0x01;
+    static const unsigned int LCD_RETURNHOME = 0x02;
+    static const unsigned int LCD_ENTRYMODESET = 0x04;
+    static const unsigned int LCD_DISPLAYCONTROL = 0x08;
+    static const unsigned int LCD_CURSORSHIFT = 0x10;
+    static const unsigned int LCD_FUNCTIONSET = 0x20;
+    static const unsigned int LCD_SETCGRAMADDR = 0x40;
+    static const unsigned int LCD_SETDDRAMADDR = 0x80;
+
+    // flags for display entry mode
+    static const unsigned int LCD_ENTRYRIGHT = 0x00;
+    static const unsigned int LCD_ENTRYLEFT = 0x02;
+    static const unsigned int LCD_ENTRYSHIFTINCREMENT = 0x01;
+    static const unsigned int LCD_ENTRYSHIFTDECREMENT = 0x00;
+
+    // flags for display on/off control
+    static const unsigned int LCD_DISPLAYON = 0x04;
+    static const unsigned int LCD_DISPLAYOFF = 0x00;
+    static const unsigned int LCD_CURSORON = 0x02;
+    static const unsigned int LCD_CURSOROFF = 0x00;
+    static const unsigned int LCD_BLINKON = 0x01;
+    static const unsigned int LCD_BLINKOFF = 0x00;
+
+    // flags for display/cursor shift
+    static const unsigned int LCD_DISPLAYMOVE = 0x08;
+    static const unsigned int LCD_CURSORMOVE = 0x00;
+    static const unsigned int LCD_MOVERIGHT = 0x04;
+    static const unsigned int LCD_MOVELEFT = 0x00;
+
+    // flags for function set
+    static const unsigned int LCD_8BITMODE = 0x10;
+    static const unsigned int LCD_4BITMODE = 0x00;
+    static const unsigned int LCD_2LINE = 0x08;
+    static const unsigned int LCD_1LINE = 0x00;
+    static const unsigned int LCD_5x10DOTS = 0x04;
+    static const unsigned int LCD_5x8DOTS = 0x00;
+
+    // flags for backlight control
+    static const unsigned int LCD_BACKLIGHT = 0x08;
+    static const unsigned int LCD_NOBACKLIGHT = 0x00;
+
+    static const unsigned int En = 0b00000100; // Enable bit
+    static const unsigned int Rw = 0b00000010; // Read/Write bit
+    static const unsigned int Rs = 0b00000001; // Register select bit
+
 public:
   LiquidCrystal_I2C(uint8_t lcd_Addr,uint8_t lcd_cols,uint8_t lcd_rows);
   void begin(uint8_t cols, uint8_t rows, uint8_t charsize = LCD_5x8DOTS );
